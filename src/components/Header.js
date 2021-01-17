92d828bd9da7f8a204f3c9e08d8ef7bad90c3f87
Sebastian <sebastian.calderon@sansano.usm.cl>
2021-01-17 00:41:31 -0300

    se agrego el video

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,17 +9,17 @@ import wave from '../images/wave.svg'
 function Header() {
     return (
         <header className="head">
-            <Navbar style={{ marginTop: 80,paddingTop:0,paddingBottom:0 }} bg="light" variant="light">
+            <Navbar style={{ marginTop: 40,paddingTop:0,paddingBottom:0,paddingLeft:0 }} bg="light" variant="light">
                 <button id="nav-button" class="nav-trigger">
                     <span></span>
                     <span></span>
                     <span></span>
                 </button>
-
-                <div className="imageFrame">
+             
                   <img id ="wave" src= {wave} aria-hidden="true" class="img-fluid"></img>
                   <img id="title" src={title} aria-hidden="true" class="img-fluid"></img>
-                </div>
+                
+        
             </Navbar>
         </header>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,8 +9,11 @@ function Main(){
         <div className="container-fluid px-0">
             <Header></Header>
             <div className="content">
-                
-            </div>
+                <div className="video">
+                    <div className="embed-responsive embed-responsive-4by3">
+                     <iframe width="560" height="315" src="https://www.youtube.com/embed/CghQjc2vDrY" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                 </div>
+                 </div>
+                </div>
         </div>
     )
     ;
